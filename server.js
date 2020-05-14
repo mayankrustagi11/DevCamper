502e4892adf5786370177a985d0c750c6581d0aa
Rustagi <marustagi@deloitte.com>
2020-05-15 00:17:09 +0530

    MongoDB and Colors

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Routes
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env variables
 dotenv.config({path: './config/config.env'});
+
+// Connect to database
+connectDB();
+
+// Routes
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -19,5 +24,10 @@ if(process.env.NODE_ENV === 'development') {
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    server.close(() => process.exit(1));
+});
