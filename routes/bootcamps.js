46cd62e6e1156d3833584fcdd9b7a92344238425
Rustagi <marustagi@deloitte.com>
2020-05-16 22:02:23 +0530

    Image Upload

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp, getBootcampsInRadius } = require('../controllers/bootcamps');
+const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp, getBootcampsInRadius, bootcampPhotoUpload } = require('../controllers/bootcamps');
 
 // Include other resource routers
 const courseRouter = require('./courses');
@@ -11,6 +11,9 @@ router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance')
     .get(getBootcampsInRadius);
+
+router.route('/:id/photo')
+    .put(bootcampPhotoUpload);
 
 router.route('/')
     .get(getBootcamps)
