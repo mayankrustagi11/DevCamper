a5359f28e7505ed465cc24de4bef65b10b49bc49
Mayank Rustagi <marustagi@deloitte.com>
2020-05-18 00:04:06 +0530

    Reviews, Ratings and Logout

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,15 +9,13 @@ exports.protect = asyncHandler(async (req, res, next) => {
 
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         token = req.headers.authorization.split(' ')[1];
-    } else if(req.cookies.token) {
+    }/*  else if(req.cookies.token) {
         token = req.cookies.token;
-    }
-
+    } */
     // Make sure token exists
     if(!token) {
         return next(new ErrorResponse('Not authorized', 401));
     }
-
     try {
         // Verify token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
